aeee03c3df980a8919fc0477a8608b6f3692855d
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-06 20:31:30 +0545

    [Added] dummy blogs

--- src/components/Blogs/javascripts/ListBlogs.js
+++ src/components/Blogs/javascripts/ListBlogs.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "../css/blog.css";
-import { Search } from "semantic-ui-react";
-
+import { Grid, Image, Segment } from "semantic-ui-react";
 function ListBlogs() {
   return (
     <div>
       <div className="bloglist">
         <h1>Explore Blogs</h1>
-        <div></div>
+        <div>
+          <Image
+            style={{ width: 300, height: 300, padding: 20 }}
+            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+          />
+          <Image
+            style={{ width: 300, height: 300, padding: 20 }}
+            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+          />
+          <Image
+            style={{ width: 300, height: 300, padding: 20 }}
+            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+          />
+          <Image
+            style={{ width: 300, height: 300, padding: 20 }}
+            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+          />
+          <Image
+            style={{ width: 300, height: 300, padding: 20 }}
+            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+          />
+          <Image
+            style={{ width: 300, height: 300, padding: 20 }}
+            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+          />
+        </div>
       </div>
     </div>
   );

9550a48c93efddf970745fba9c634537f46564e3
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-06 21:29:25 +0545

    [Updated] blogs for blog page

--- src/components/Blogs/javascripts/ListBlogs.js
+++ src/components/Blogs/javascripts/ListBlogs.js
@@ -6,8 +6,98 @@ function ListBlogs() {
     <div>
       <div className="bloglist">
         <h1>Explore Blogs</h1>
-        <div>
-          <Image
+        <div className="blogfile">
+          <div className="indblog">
+            <Image
+              style={{ width: 300, height: 300, borderRadius: 40 }}
+              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+            />
+            <br />
+            <label>Blog Post 1</label>
+            <br />
+            <br />
+            <q>
+              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
+              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
+            </q>
+            <br />
+            <br />
+            <a href="#">See Blog</a>
+          </div>
+
+          <div className="indblog">
+            <Image
+              style={{ width: 300, height: 300, borderRadius: 40 }}
+              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+            />
+            <br />
+            <label>Blog Post 1</label>
+            <br />
+            <br />
+            <q>
+              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
+              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
+            </q>
+            <br />
+            <br />
+            <a href="#">See Blog</a>
+          </div>
+
+          <div className="indblog">
+            <Image
+              style={{ width: 300, height: 300, borderRadius: 40 }}
+              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+            />
+            <br />
+            <label>Blog Post 1</label>
+            <br />
+            <br />
+            <q>
+              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
+              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
+            </q>
+            <br />
+            <br />
+            <a href="#">See Blog</a>
+          </div>
+
+          <div className="indblog">
+            <Image
+              style={{ width: 300, height: 300, borderRadius: 40 }}
+              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+            />
+            <br />
+            <label>Blog Post 1</label>
+            <br />
+            <br />
+            <q>
+              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
+              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
+            </q>
+            <br />
+            <br />
+            <a href="#">See Blog</a>
+          </div>
+
+          <div className="indblog">
+            <Image
+              style={{ width: 300, height: 300, borderRadius: 40 }}
+              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+            />
+            <br />
+            <label>Blog Post 1</label>
+            <br />
+            <br />
+            <q>
+              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
+              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
+            </q>
+            <br />
+            <br />
+            <a href="#">See Blog</a>
+          </div>
+
+          {/* <Image
             style={{ width: 300, height: 300, padding: 20 }}
             src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
           />
@@ -30,7 +120,7 @@ function ListBlogs() {
           <Image
             style={{ width: 300, height: 300, padding: 20 }}
             src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          />
+          /> */}
         </div>
       </div>
     </div>

3948c31ef3565e1fd02c58138a7408f75bf043f7
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-07 17:21:54 +0545

    [Updated] axios with cors headers

--- src/components/Blogs/javascripts/ListBlogs.js
+++ src/components/Blogs/javascripts/ListBlogs.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/blog.css";
 import { Grid } from "@material-ui/core";
 import Axios from "axios";
 
 function ListBlogs() {
-  React.useEffect(() => {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
     Axios.get("http://127.0.0.1:8000/create/")
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
+        setPost(res.data);
       })
       .catch((error) => {
         console.log(error);
@@ -20,23 +23,25 @@ function ListBlogs() {
         <h1>Explore Blogs</h1>
         <div className="blogfile">
           <Grid container>
-            <div className="indblog">
-              <img
-                style={{ width: 300, height: 300, borderRadius: 40 }}
-                src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-              />
-              <br />
-              <label>Blog Post 1</label>
-              <br />
-              <br />
-              <q>
-                First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
-                sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
-              </q>
-              <br />
-              <br />
-              <a href="#">See Blog</a>
-            </div>
+            {post.map((post) => (
+              <div className="indblog">
+                <img
+                  style={{ width: 300, height: 300, borderRadius: 40 }}
+                  src={post.thumbnail}
+                  alt="Blog Image"
+                />
+                <br />
+                <label>{post.title}</label>
+                <br />
+                <br />
+                <q>{post.subtitle}</q>
+                <br />
+                <br />
+                {post.category}
+                {post.date_posted}
+                <a href="#">See Blog</a>
+              </div>
+            ))}
           </Grid>
         </div>
       </div>

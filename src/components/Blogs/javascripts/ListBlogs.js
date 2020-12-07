4e2f9e55715356e3993b2f83b947e3b2c0089f60
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-07 14:29:26 +0545

    [Updated] blog basic design

--- src/components/Blogs/javascripts/ListBlogs.js
+++ src/components/Blogs/javascripts/ListBlogs.js
@@ -1,126 +1,32 @@
 import React from "react";
 import "../css/blog.css";
-import { Grid, Image, Segment } from "semantic-ui-react";
+import { Grid } from "@material-ui/core";
+
 function ListBlogs() {
   return (
     <div>
       <div className="bloglist">
         <h1>Explore Blogs</h1>
         <div className="blogfile">
-          <div className="indblog">
-            <Image
-              style={{ width: 300, height: 300, borderRadius: 40 }}
-              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-            />
-            <br />
-            <label>Blog Post 1</label>
-            <br />
-            <br />
-            <q>
-              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
-              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
-            </q>
-            <br />
-            <br />
-            <a href="#">See Blog</a>
-          </div>
-
-          <div className="indblog">
-            <Image
-              style={{ width: 300, height: 300, borderRadius: 40 }}
-              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-            />
-            <br />
-            <label>Blog Post 1</label>
-            <br />
-            <br />
-            <q>
-              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
-              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
-            </q>
-            <br />
-            <br />
-            <a href="#">See Blog</a>
-          </div>
-
-          <div className="indblog">
-            <Image
-              style={{ width: 300, height: 300, borderRadius: 40 }}
-              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-            />
-            <br />
-            <label>Blog Post 1</label>
-            <br />
-            <br />
-            <q>
-              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
-              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
-            </q>
-            <br />
-            <br />
-            <a href="#">See Blog</a>
-          </div>
-
-          <div className="indblog">
-            <Image
-              style={{ width: 300, height: 300, borderRadius: 40 }}
-              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-            />
-            <br />
-            <label>Blog Post 1</label>
-            <br />
-            <br />
-            <q>
-              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
-              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
-            </q>
-            <br />
-            <br />
-            <a href="#">See Blog</a>
-          </div>
-
-          <div className="indblog">
-            <Image
-              style={{ width: 300, height: 300, borderRadius: 40 }}
-              src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-            />
-            <br />
-            <label>Blog Post 1</label>
-            <br />
-            <br />
-            <q>
-              First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
-              sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
-            </q>
-            <br />
-            <br />
-            <a href="#">See Blog</a>
-          </div>
-
-          {/* <Image
-            style={{ width: 300, height: 300, padding: 20 }}
-            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          />
-          <Image
-            style={{ width: 300, height: 300, padding: 20 }}
-            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          />
-          <Image
-            style={{ width: 300, height: 300, padding: 20 }}
-            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          />
-          <Image
-            style={{ width: 300, height: 300, padding: 20 }}
-            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          />
-          <Image
-            style={{ width: 300, height: 300, padding: 20 }}
-            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          />
-          <Image
-            style={{ width: 300, height: 300, padding: 20 }}
-            src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
-          /> */}
+          <Grid container>
+            <div className="indblog">
+              <img
+                style={{ width: 300, height: 300, borderRadius: 40 }}
+                src="https://i.pinimg.com/236x/fb/c7/ea/fbc7eaf53db67c1fb82bade23913b667.jpg"
+              />
+              <br />
+              <label>Blog Post 1</label>
+              <br />
+              <br />
+              <q>
+                First blog in Blogs Page ehdjssjfcdsjhfsd sjdhfsjhfshjfshf
+                sdhjcsjhffhj hdghsjedwejdsjed sjhdjshsejfdhsejd jsedhjshfjefhkej
+              </q>
+              <br />
+              <br />
+              <a href="#">See Blog</a>
+            </div>
+          </Grid>
         </div>
       </div>
     </div>

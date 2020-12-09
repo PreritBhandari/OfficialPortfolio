a42a068aa580ce2f1ea06c21b8d56ba9b7e601d2
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-09 20:14:54 +0545

    [Updated] suggested blogs

--- src/components/Blogs/javascripts/DetailBlog.js
+++ src/components/Blogs/javascripts/DetailBlog.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/blog.css";
+import Axios from "axios";
+import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
+// import { Link } from "react-router-dom";
 
 import moment from "moment";
 
@@ -12,29 +15,77 @@ function DetailBlog(props) {
     category,
     date_posted,
   } = props.location.state;
+
+  const [similar, setSimilar] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`https://preritbhandari.pythonanywhere.com/list/${category}/`)
+      .then((res) => {
+        console.log(res.data);
+        setSimilar(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [category]);
   return (
-    <div
-      style={{
-        backgroundImage: ` url(${thumbnail})`,
-      }}
-      className="indblog detailblog"
-    >
-      <div className="sideData">
-        <label>{title}</label> &nbsp;&nbsp; || &nbsp;&nbsp;
-        <category>Category: {category} &nbsp;&nbsp;</category>
-        <div className="date2">
-          Posted at : {moment(date_posted).format("MMMM Do YYYY, h:mm:ss a")}
+    <div>
+      <div
+        style={{
+          backgroundImage: ` url(${thumbnail})`,
+        }}
+        className="indblog detailblog"
+      >
+        <div className="sideData">
+          <label>{title}</label> &nbsp;&nbsp; || &nbsp;&nbsp;
+          <category>Category: {category} &nbsp;&nbsp;</category>
+          <div className="date2">
+            Posted at : {moment(date_posted).format("MMMM Do YYYY, h:mm:ss a")}
+          </div>
+          <hr
+            style={{
+              width: "110%",
+              paddingBottom: 1,
+              marginBottom: 75,
+            }}
+          />
+          <div className="post">{post}</div>
+          <br />
+          <br />
         </div>
-        <hr
-          style={{
-            width: "110%",
-            paddingBottom: 1,
-            marginBottom: 75,
-          }}
-        />
-        <div className="post">{post}</div>
-        <br />
-        <br />
+      </div>
+
+      <div className="similar">
+        <similar>Similar Blogs</similar>
+        <div className="similarblog">
+          <GridList cols={2.5}>
+            {similar.map((data) => (
+              <GridListTile className="suggestion" key={data.id}>
+                {/* <Link
+                  to={{
+                    pathname: "/detailblog",
+                    state: {
+                      id: data.id,
+                      title: data.title,
+                      subtitle: data.subtitle,
+                      thumbnail: data.thumbnail,
+                      category: data.category,
+                      post: data.post,
+                      date_posted: data.date_posted,
+                    },
+                  }}
+                > */}
+                <img src={data.thumbnail} alt={data.title} />
+                {/* </Link> */}
+
+                <GridListTileBar
+                  style={{ fontFamily: "sans-serif" }}
+                  title={data.title}
+                />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
       </div>
     </div>
   );
